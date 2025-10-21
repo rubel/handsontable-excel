358190804463b4f135f082004f3217607c074218
rubel <rubel.xpress@gmail.com>
2025-10-21 23:40:23 +0600

    init

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import HandsontableDemo from "@/components/tandsontable";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>Hi</main>
+    <div>
+      <HandsontableDemo />
     </div>
   );
 }
